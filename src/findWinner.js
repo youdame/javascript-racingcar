5b19c9ce48d47124649f75bd308a4753e31a58f7
youdame <112458620+youdame@users.noreply.github.com>
2025-02-18 12:16:24 +0900

    feat: 수업 코드 업로드

--- src/findWinner.js
+++
@@ -1,6 +0,0 @@
-const findWinner = (cars) => {
-  const winningPosition = Math.max(...cars.map((car) => car.position));
-  return cars.filter((car) => car.position === winningPosition);
-};
-
-export default findWinner;
